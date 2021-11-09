137f7b63614c41e258d944fc0a4a0fa6953656bb
Mateo sierra <m.sierra1037@hotmail.com>
2021-11-08 20:17:08 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+require('dotenv').config({path:"contentful.env"})
+
+const { 
+  access_token : accessToken, 
+  space_id : spaceId } = process.env
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
@@ -7,8 +13,8 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: "\u0016",
-        spaceId: "",
+        accessToken,
+        spaceId,
       },
     },
   ],
